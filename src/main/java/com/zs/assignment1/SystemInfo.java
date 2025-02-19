a4ba06a7644d9aa728ce2849df04973f0a4022f2
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-19 16:20:37 +0530

    Assignment 1 | Implemented functions to get System Info

---
+++ src/main/java/com/zs/assignment1/SystemInfo.java
@@ -0,0 +1,28 @@
+package com.zs.assignment1;
+import com.zs.assignment1.system.SystemDetails;
+
+
+public class SystemInfo {
+    public static void main(String[] args) {
+        SystemDetails systemDetails= new SystemDetails();
+
+        String currentUser= systemDetails.getUser();
+        String homeDirectory= systemDetails.getHomeDirectory();
+        int systemMemory= systemDetails.getSystemMemory();
+        int systemCores= systemDetails.getSystemCores();
+        int systemDiskSize= systemDetails.getSystemDiskSize();
+        String osBuild = systemDetails.getOsBuild();
+        String osVersion= systemDetails.getOsVersion();
+
+        System.out.println("Name of user is: " + currentUser);
+        System.out.println("Home Directory is: " + homeDirectory);
+        System.out.println("System memory size is: " + systemMemory + " GB");
+        System.out.println("System cores count is: " + systemCores );
+        System.out.println("System disk size is: " + systemDiskSize + " GB");
+        System.out.println("OS Build is: " + osBuild);
+        System.out.println("OS Version is: " + osVersion);
+
+
+
+    }
+}
---
+++ src/main/java/com/zs/assignment1/system/SystemDetails.java
@@ -0,0 +1,38 @@
+package com.zs.assignment1.system;
+
+import java.io.File;
+
+import java.lang.management.ManagementFactory;
+import com.sun.management.OperatingSystemMXBean;
+
+public class SystemDetails {
+    OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    File root = new File("/");
+
+    public String getUser(){
+        return System.getProperty("user.name");
+    }
+    public String getHomeDirectory(){
+        return System.getProperty("user.home");
+    }
+    public String getOsVersion(){
+        return System.getProperty("os.version");
+    }
+    public String getOsBuild(){
+        return System.getProperty("os.name");
+    }
+
+    public int getSystemMemory(){
+        long totalMemoryInBytes = osBean.getTotalPhysicalMemorySize();
+        int totalMemoryInGB = (int) (totalMemoryInBytes/(1024*1024*1024));
+        return totalMemoryInGB;
+    }
+    public int getSystemDiskSize(){
+        long totalDiskSpaceInBytes = root.getTotalSpace();
+        int totalDiskSpaceInGB = (int)(totalDiskSpaceInBytes/(1024*1024*1024));
+        return totalDiskSpaceInGB;
+    }
+    public int getSystemCores(){
+        return osBean.getAvailableProcessors();
+    }
+}
