1cd5d4e55eee8036f72a57a9b456696e84c9ab94
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-23 16:21:28 +0530

    Assignment 3 | AB | added the main function to run the project

--- src/main/java/com/zs/assignments/assignment3/Catalogue.java
+++ src/main/java/com/zs/assignments/assignment3/Catalogue.java
@@ -1,7 +1,18 @@
 package com.zs.assignments.assignment3;
 
+import com.zs.assignments.assignment3.controller.Controller;
+import com.zs.assignments.assignment3.models.Category;
+import com.zs.assignments.assignment3.services.CatalogueServices;
+
+import java.util.ArrayList;
+
 public class Catalogue {
     public static void main(String[] args) {
-        System.out.println("hi");
+        CatalogueServices catalogueServices = new CatalogueServices();
+        Controller controller = new Controller();
+
+        ArrayList<Category> catalogue = catalogueServices.dummyCatalogue();
+
+        controller.control(catalogue);
     }
 }
