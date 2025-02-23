155f6cc179617f7659c826fc4b46894c0abf6775
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-23 16:19:50 +0530

    Assignment 3 | AB | added methods related to Category

---
+++ src/main/java/com/zs/assignments/assignment3/services/CategoryServices.java
@@ -0,0 +1,60 @@
+package com.zs.assignments.assignment3.services;
+
+import com.zs.assignments.assignment3.models.Category;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class CategoryServices {
+    Scanner sc = new Scanner(System.in);
+
+    public void addCategory(ArrayList<Category> catalogue) {
+
+        System.out.println("Enter the new category name: ");
+        String categoryName = sc.next();
+
+        for (Category currentCategory : catalogue) {
+            if (Objects.equals(currentCategory.getName(), categoryName)) {
+                System.out.println("Category already exist");
+                return;
+            }
+        }
+        Category newCategory = new Category(categoryName);
+        catalogue.add(newCategory);
+    }
+
+    public void deleteCategory(ArrayList<Category> catalogue) {
+
+        System.out.println("Enter the category name: ");
+        String categoryName = sc.next();
+
+        for (Category currentCategory : catalogue) {
+            if (Objects.equals(currentCategory.getName(), categoryName)) {
+                catalogue.remove(currentCategory);
+                System.out.println("Category " + categoryName + "deleted succesfully");
+                return;
+            }
+        }
+        System.out.println("Category do not exist");
+    }
+
+    public boolean isCategoryExist(ArrayList<Category> allCategoriesList, String categoryName) {
+
+        for (Category currentCategory : allCategoriesList) {
+            if (Objects.equals(currentCategory.getName(), categoryName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Category getCategory(ArrayList<Category> allCategoriesList, String categoryName) {
+        for (Category currentCategory : allCategoriesList) {
+            if (Objects.equals(currentCategory.getName(), categoryName)) {
+                return currentCategory;
+            }
+        }
+        return new Category("NULL");
+    }
+}
