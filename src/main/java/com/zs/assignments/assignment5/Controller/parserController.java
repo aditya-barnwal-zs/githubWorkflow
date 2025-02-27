fc5a1d4200f4dcaade2b59aff0e114bef53994b4
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-27 13:02:09 +0530

    Assignment 5 | AB | created parser to read file

--- src/main/java/com/zs/assignments/assignment5/Controller/parserController.java
+++ src/main/java/com/zs/assignments/assignment5/Controller/parserController.java
@@ -1,4 +1,19 @@
 package com.zs.assignments.assignment5.Controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class parserController {
+    public void runParser(String filePath){
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());;
+        }
+    }
+
 }
