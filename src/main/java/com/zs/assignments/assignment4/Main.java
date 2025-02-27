761fccd925f205fdae6357a9b7c3463f39b252e0
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-27 18:56:49 +0530

    Assignment 4 | AB | corrected the format

--- src/main/java/com/zs/assignments/assignment4/Main.java
+++ src/main/java/com/zs/assignments/assignment4/Main.java
@@ -4,7 +4,6 @@ import com.zs.assignments.assignment3.models.Category;
 import com.zs.assignments.assignment3.models.SubCategory;
 import com.zs.assignments.assignment4.controller.LRUController;
 import com.zs.assignments.assignment4.util.CatalogGenerator;
-import lombok.experimental.Helper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,14 +16,12 @@ public class Main {
     public static void main(String[] args) {
 
         LOGGER.info("Program started");
-
-        CatalogGenerator catalogGenerator= new CatalogGenerator();
+        CatalogGenerator catalogGenerator = new CatalogGenerator();
         ArrayList<Category> catalogue = catalogGenerator.dummyCatalog();
 
         int capacity = 3;
         LinkedHashMap<String, Category> categoryLRU = new LinkedHashMap<>(capacity, 0.5f, true);
         LinkedHashMap<String, SubCategory> subCategoryLRU = new LinkedHashMap<>(capacity, 0.5f, true);
-
 
         LRUController controller = new LRUController();
         controller.runProgram(catalogue, categoryLRU, subCategoryLRU, capacity);
