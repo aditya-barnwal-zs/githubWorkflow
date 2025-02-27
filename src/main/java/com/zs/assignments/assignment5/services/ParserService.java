feb14d64987b5c41041b62036ca71a0f6a2e9c3d
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-02-27 15:05:15 +0530

    Assignment 5 | AB | added service to check format of Message line using regex

--- src/main/java/com/zs/assignments/assignment5/services/ParserService.java
+++ src/main/java/com/zs/assignments/assignment5/services/ParserService.java
@@ -21,4 +21,9 @@ public class ParserService {
         String dateRegex="Date:   \\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\\b \\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\b \\b(0[1-9]|[1-2][0-9]|3[0-1])\\b \\b([0-1][0-9]|[2][2-4])\\b:[0-5][0-9]:[0-5][0-9] [1-9][0-9]{3} \\+0530";
         return Pattern.matches(dateRegex, date);
     }
+
+    public Boolean checkmessage(String message){
+        String messageRegex="    .+";
+        return Pattern.matches(messageRegex, messageRegex);
+    }
 }
