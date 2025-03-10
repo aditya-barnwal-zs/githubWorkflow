ed36bf2e5bdd06128921b375c13c882cfdc79081
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-03-10 16:47:20 +0530

    Assignment 10 | AB | project Inialized

---
+++ src/main/java/com/zs/assignments/assignment10/ManageProducts.java
@@ -0,0 +1,10 @@
+package com.zs.assignments.assignment10;
+
+import com.zs.assignments.assignment10.controllers.ProductController;
+
+public class ManageProducts {
+    public static void main(String[] args) {
+        ProductController productController = new ProductController();
+        productController.applyOperations();
+    }
+}
