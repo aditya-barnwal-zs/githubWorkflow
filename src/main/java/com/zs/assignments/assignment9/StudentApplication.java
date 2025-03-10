276799fcd25e77aa7da523d71c923d2115c3944b
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-03-10 15:10:44 +0530

    Assignment 9 | AB | class name changed

--- src/main/java/com/zs/assignments/assignment9/StudentApplication.java
+++ src/main/java/com/zs/assignments/assignment9/StudentApplication.java
@@ -3,7 +3,7 @@ package com.zs.assignments.assignment9;
 
 import com.zs.assignments.assignment9.controllers.StudentController;
 
-public class MainApplication {
+public class StudentApplication {
     public static void main(String[] args) {
         StudentController studentController = new StudentController();
         studentController.runProgram();
