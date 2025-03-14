d71642735a6cd49979ce28cfe5e7707eaa6b5e62
Aditya Barnwal <aditya.barnwal@zopsmart.com>
2025-03-15 02:06:09 +0530

    Assignment 11-12 | AB | project initialized

---
+++ src/main/java/com/zs/assignments/assignment11/Main.java
@@ -0,0 +1,13 @@
+package com.zs.assignments.assignment11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+//        System.out.println("Hello world");
+//        http://localhost:8082/swagger-ui.html
+    }
+}
